4aa54b49d80b3fd877d3c9137af4b89b7c166ec1
Maria Mironova <maria@example.com>
2022-04-21 02:03:17 +0300

    add nojekyll file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/pipe',
+  assetPrefix: '/pipe',
   reactStrictMode: true,
   env: {
-    API_URL: 'http://localhost:4200'
-  }
+    API_URL: 'http://localhost:4200',
+  },
 }
 
 module.exports = nextConfig
