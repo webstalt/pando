78c5510bc279a2e8041c447eeabeec1231a92644
Maria Mironova <maria@example.com>
2022-04-27 15:07:05 +0300

    styled button, refactor

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,4 +1,5 @@
 import { StyledLink } from '../styledLink/StyledLink'
+import { Button } from '../button/Button'
 
 import classes from './header.module.scss'
 
@@ -6,118 +7,115 @@ import Web3 from 'web3'
 import stakingContract from '../../blockchain/contract.js'
 import { useState, useEffect } from 'react'
 
-
-
-
 export function Header() {
+  /* ---------Web3--------- */
+  const [web3, setWeb3] = useState({})
+  const [address, setAddress] = useState({})
+  const [vmContract, setVmContract] = useState(null)
+  const [showConnect, setShowConnect] = useState(true)
+  const [switchNetwork, setSwitchNetwork] = useState(true)
 
-/* ---------Web3--------- */
-const [web3, setWeb3] = useState({})
-const [address, setAddress] = useState({});
-const [vmContract, setVmContract] = useState(null)
-const [showConnect, setShowConnect] = useState(true);
-const [switchNetwork, setSwitchNetwork] = useState(true);
+  /* ---------UseEffects--------- */
+  //Run these functions if either address or vmContract changes.
+  useEffect(() => {
+    if (vmContract && address) setShowConnect(false)
+  }, [vmContract, address])
 
-
-
-/* ---------UseEffects--------- */
-//Run these functions if either address or vmContract changes.
-useEffect(() => {
-  
-  if (vmContract && address) setShowConnect(false)
-
-}, [vmContract, address])
-
-
-const connectWalletHandler = async () => {
-  console.log("connectWalletHandler Called")
-  if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
-      console.log("MetaMask Detected")
+  const connectWalletHandler = async () => {
+    console.log('connectWalletHandler Called')
+    if (
+      typeof window !== 'undefined' &&
+      typeof window.ethereum !== 'undefined'
+    ) {
+      console.log('MetaMask Detected')
       try {
-          await window.web3.currentProvider.enable();
-          web3 = new Web3(window.ethereum)
-          setWeb3(web3)
-          //console.log("web3 set")
+        await window.web3.currentProvider.enable()
+        web3 = new Web3(window.ethereum)
+        setWeb3(web3)
+        //console.log("web3 set")
 
-          const accounts = await web3.eth.getAccounts() //Get list of accounts associated with the wallet
-          setAddress(accounts[0])
-          //console.log("Account address set")
+        const accounts = await web3.eth.getAccounts() //Get list of accounts associated with the wallet
+        setAddress(accounts[0])
+        //console.log("Account address set")
 
-          const vm = stakingContract(web3)
-          setVmContract(vm)
+        const vm = stakingContract(web3)
+        setVmContract(vm)
 
-          
-          if (window.ethereum.networkVersion == "4") { //TODO: Change chain ID, currently set to Rinkeby network
-              console.log(window.ethereum.networkVersion, 'window.ethereum.networkVersion');
-              setSwitchNetwork(false)
-
-          }
-          else {
-              console.log('ERROR: Initial window.ethereum.networkVersion:', window.ethereum.networkVersion);
-              setSwitchNetwork(true)
-
-          }
+        if (window.ethereum.networkVersion == '4') {
+          //TODO: Change chain ID, currently set to Rinkeby network
+          console.log(
+            window.ethereum.networkVersion,
+            'window.ethereum.networkVersion'
+          )
+          setSwitchNetwork(false)
+        } else {
+          console.log(
+            'ERROR: Initial window.ethereum.networkVersion:',
+            window.ethereum.networkVersion
+          )
+          setSwitchNetwork(true)
+        }
       } catch (err) {
-          console.log(err.message)
+        console.log(err.message)
       }
-
+    } else {
+      alert('Please Install The Metamask Wallet')
+    }
   }
-  else {
-      alert("Please Install The Metamask Wallet")
-  }
 
-}
-
-//Event listner for address changes
-const addressChangedListener = async () => {
-  window.ethereum.on('accountsChanged', (accounts) => {
+  //Event listner for address changes
+  const addressChangedListener = async () => {
+    window.ethereum.on('accountsChanged', (accounts) => {
       // Handle the new accounts, or lack thereof.
       // "accounts" will always be an array, but it can be empty.
       setAddress(accounts[0])
-      console.log("Account changed to:", accounts[0])
+      console.log('Account changed to:', accounts[0])
       //Check if a user has disconnected all addresses from the website
-      if(accounts[0] == null){
+      if (accounts[0] == null) {
         setShowConnect(true)
-        console.log("User disconnected all addresses")
+        console.log('User disconnected all addresses')
       }
-  });
-}
+    })
+  }
 
-
-//Event listener for chainId changes
-const chainChangedListener = async () => {
+  //Event listener for chainId changes
+  const chainChangedListener = async () => {
     //console.log("chainChangedListener Called: ", window.ethereum.networkVersion)
     ethereum.on('chainChanged', (chainId) => {
-        //console.log("Inner chain:", chainId)
-        if (chainId != "0x4") {
-            setSwitchNetwork(true)
-            console.log("setting Switch Network to True")
-            
-        }
-        else {
-            setSwitchNetwork(false)
-            console.log("setting Switch Network to False")
-        }
-    });
-}
+      //console.log("Inner chain:", chainId)
+      if (chainId != '0x4') {
+        setSwitchNetwork(true)
+        console.log('setting Switch Network to True')
+      } else {
+        setSwitchNetwork(false)
+        console.log('setting Switch Network to False')
+      }
+    })
+  }
 
-//Handle the change of network
-const switchNetworkHandler = async () => {
-  try {
+  //Handle the change of network
+  const switchNetworkHandler = async () => {
+    try {
       const result = await ethereum.request({
-          method: 'wallet_switchEthereumChain',
-          params: [{ chainId: '0x4' }], //TODO: Change before deployment
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x4' }], //TODO: Change before deployment
       })
       if (result == null) {
-          //console.log("Network Set to Rinkeby") //TODO: Change before deployment
-          setSwitchNetwork(false)
-          
+        //console.log("Network Set to Rinkeby") //TODO: Change before deployment
+        setSwitchNetwork(false)
       }
-  } catch (err) {
+    } catch (err) {
       console.log(err)
+    }
   }
-}
 
+  const copyToClipboardToast = () => {} // added to escape the error
+
+  const connectWalletButtonClickHandler = async () => {
+    await connectWalletHandler()
+    await addressChangedListener()
+    await chainChangedListener()
+  }
 
   return (
     <div className={classes.header}>
@@ -134,25 +132,36 @@ const switchNetworkHandler = async () => {
         </StyledLink>
         <StyledLink href="/about">About</StyledLink>
         <StyledLink href="/contacts">Contacts</StyledLink>
-        
-        
-        {showConnect ?
-          <button onClick={function (event) { connectWalletHandler(); addressChangedListener(); chainChangedListener(); }}
-              className="nav_button_connect">Connect Wallet
-          </button> :
 
+        {showConnect ? (
+          <Button
+            onClick={connectWalletButtonClickHandler}
+            className="nav_button_connect"
+          >
+            Connect Wallet
+          </Button>
+        ) : (
           <div className="nav_button_connect_switched">
-              {switchNetwork ?
-                  <button className="nav_button_network" onClick={switchNetworkHandler}>Switch Network</button> :
-
-                  <button onClick={() => { copyToClipboardToast(); }}
-                      className="nav_button_address">{address.substring(0, 5).concat("...").concat(address.substring(38, 43))}
-                  </button>
-
-              }
-
+            {switchNetwork ? (
+              <Button
+                className="nav_button_network"
+                onClick={switchNetworkHandler}
+              >
+                Switch Network
+              </Button>
+            ) : (
+              <Button
+                onClick={copyToClipboardToast}
+                className="nav_button_address"
+              >
+                {address
+                  .substring(0, 5)
+                  .concat('...')
+                  .concat(address.substring(38, 43))}
+              </Button>
+            )}
           </div>
-        }
+        )}
       </div>
     </div>
   )
