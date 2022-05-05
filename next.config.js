b9175d1a2fc8c7c06a805dd842a832b9cc8caea1
Maria Mironova <maria@example.com>
2022-05-06 00:15:48 +0300

    change sizes

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = withImages({
   images: {
     disableStaticImages: true,
     loader: 'akamai',
-    path: '',
+    basePath: '',
   },
   env: {
     API_URL: 'http://localhost:4200',
