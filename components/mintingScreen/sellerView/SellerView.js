6782b03439420e047ab18b42e522c1a6cda7e4e6
Maria Mironova <maria@example.com>
2022-05-14 21:47:52 +0300

    Setup steps changing

--- components/mintingScreen/sellerView/SellerView.js
+++ components/mintingScreen/sellerView/SellerView.js
@@ -1,21 +1,29 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { useSelector } from 'react-redux'
 
 import 'react-step-progress-bar/styles.css'
 import { ProgressBar, Step } from 'react-step-progress-bar'
+import { useEffect, useState } from 'react'
 
-import { ConnectWalletButton } from '../../connectWalletButton/ConnectWalletButton'
+import { Step1 } from './step1/Step1'
 import classes from './sellerView.module.scss'
 
 export function SellerView() {
   const isWalletConnected = useSelector((state) => state.user.isWalletConnected)
+  const [percentageComplete, setPercentageComplete] = useState(0)
+  const isNftFormSubmitted = false
+
+  useEffect(() => {
+    if (isNftFormSubmitted) {
+      setPercentageComplete(25)
+    }
+  }, [isWalletConnected])
 
   return (
     <>
       <div className={classes.progressBar}>
         <ProgressBar
           width="90%"
-          percent={100}
+          percent={percentageComplete}
           filledBackground="linear-gradient(to right, #66a3cd, mediumaquamarine)"
         >
           <Step transition="scale" position={0}>
@@ -47,73 +55,7 @@ export function SellerView() {
           </Step>
         </ProgressBar>
       </div>
-      <h3 className={classes.stepTitle}>Fill NFT information form</h3>
-      <Formik
-        initialValues={{ name: '', royalty: '', price: '' }}
-        validate={(values) => {
-          const errors = {}
-          if (!values.name) {
-            errors.name = 'Required'
-          }
-          if (!values.royalty) {
-            errors.royalty = 'Required'
-          }
-          if (!values.price) {
-            errors.price = 'Required'
-          }
-          return errors
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          // TODO: here will be a callback that sends data
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2))
-            setSubmitting(false)
-          }, 400)
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className={classes.form}>
-            <label className={classes.label} htmlFor="name">
-              Name
-              <Field className={classes.inputField} type="name" name="name" />
-            </label>
-            <ErrorMessage
-              className={classes.error}
-              name="name"
-              component="div"
-            />
-            <label className={classes.label} htmlFor="price">
-              Price
-              <Field className={classes.inputField} type="price" name="price" />
-            </label>
-            <ErrorMessage
-              className={classes.error}
-              name="price"
-              component="div"
-            />
-            <label className={classes.label} htmlFor="royalty">
-              Royalty
-              <Field
-                className={classes.inputField}
-                type="royalty"
-                name="royalty"
-              />
-            </label>
-            <ErrorMessage
-              className={classes.error}
-              name="royalty"
-              component="div"
-            />
-            {isWalletConnected ? (
-              <Button type="submit" disabled={isSubmitting}>
-                Submit
-              </Button>
-            ) : (
-              <ConnectWalletButton />
-            )}
-          </Form>
-        )}
-      </Formik>
+      {!isNftFormSubmitted && <Step1 isWalletConnected={isWalletConnected} />}
     </>
   )
 }
---
+++ components/mintingScreen/sellerView/step1/Step1.js
@@ -0,0 +1,80 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+
+import { ConnectWalletButton } from '../../../connectWalletButton/ConnectWalletButton'
+import { Button } from '../../../button/Button'
+
+import classes from './step1.module.scss'
+
+export function Step1({ isWalletConnected }) {
+  return (
+    <>
+      <h3 className={classes.stepTitle}>Fill NFT information form</h3>
+      <Formik
+        initialValues={{ name: '', royalty: '', price: '' }}
+        validate={(values) => {
+          const errors = {}
+          if (!values.name) {
+            errors.name = 'Required'
+          }
+          if (!values.royalty) {
+            errors.royalty = 'Required'
+          }
+          if (!values.price) {
+            errors.price = 'Required'
+          }
+          return errors
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          // TODO: here will be a callback that sends data
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            setSubmitting(false)
+          }, 400)
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className={classes.form}>
+            <label className={classes.label} htmlFor="name">
+              Name
+              <Field className={classes.inputField} type="name" name="name" />
+            </label>
+            <ErrorMessage
+              className={classes.error}
+              name="name"
+              component="div"
+            />
+            <label className={classes.label} htmlFor="price">
+              Price
+              <Field className={classes.inputField} type="price" name="price" />
+            </label>
+            <ErrorMessage
+              className={classes.error}
+              name="price"
+              component="div"
+            />
+            <label className={classes.label} htmlFor="royalty">
+              Royalty
+              <Field
+                className={classes.inputField}
+                type="royalty"
+                name="royalty"
+              />
+            </label>
+            <ErrorMessage
+              className={classes.error}
+              name="royalty"
+              component="div"
+            />
+            {isWalletConnected ? (
+              <Button type="submit" disabled={isSubmitting}>
+                Submit
+              </Button>
+            ) : (
+              <ConnectWalletButton />
+            )}
+          </Form>
+        )}
+      </Formik>
+    </>
+  )
+}
